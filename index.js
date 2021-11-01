ec8e5e0293c03970d784cabd52b46c7472617001
mazen <mak9986@hotmail.com>
2021-11-01 15:51:04 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function saturdayFun(fun = "roller-skate"){
+    return `this saturday, i want to ${fun}`;
+}
+
+function mondayWork(job = "go to office"){
+    return `This monday i will ${job}`;
+}
+
+function wrapAdjective(n = "*"){
+    let y = n;
+    return function ("n = 'special"){
+    return `${y} ${n} ${y}!`;{
+
+    }
+}
+
+
